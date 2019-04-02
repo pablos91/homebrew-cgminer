f9639eae33daba5690d360cf68c31d914e5c6082
p.niewiadomski <pawel@maple.com.pl>
2019-04-02 21:47:59 +0200

    added cgminer 3.12.3

---
+++ Formula/cgminer@3.12.3.rb
@@ -0,0 +1,42 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Cgminer < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ckolivas/cgminer/archive/v3.12.3.zip"
+  version "3.12.3"
+  sha256 ""
+  # depends_on "cmake" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "curl" => :build
+  depends_on "c-ares" => :build
+  depends_on "libusb"
+  depends_on "pkg-config" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    ENV['NOCONFIGURE'] = '1'
+
+    system "./autogen.sh"
+    system "./configure", "--enable-scrypt", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cgminer`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
